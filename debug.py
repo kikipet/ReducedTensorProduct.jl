a276bda6f27bb37d568ccbe27efe225a45c05707
kikipet <songkimwl@gmail.com>
2022-05-04 23:56:57 -0400

    Delete debug.py

--- debug.py
+++
@@ -1,7 +0,0 @@
-from e3nn.o3 import *
-# from _reduce import _wigner_nj
-from _reduce import ReducedTensorProducts
-
-irreps = {'i': Irreps('1x1o'), 'j': Irreps('1x1o')}
-
-res = ReducedTensorProducts('ijkl=jikl=ikjl=ijlk',i='1e')
